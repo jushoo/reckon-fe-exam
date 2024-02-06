728f7b05c5c196488e3f66ad6dfbd0ec363ca786
jushoo <132144846+jushoo@users.noreply.github.com>
2024-02-06 20:30:02 +0800

    Add pause and resume log

--- src/components/Log.tsx
+++ src/components/Log.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { StockPriceResponse } from "../schema/stock-price.schema";
+import { v4 } from "uuid";
 
 interface Props {
   data: StockPriceResponse | undefined;
@@ -20,7 +21,13 @@ export function Log({ data, loading, error }: Props) {
     },
   ]);
 
+  const [isPolling, setIsPolling] = useState<boolean>(true);
+
   useEffect(() => {
+    if (!isPolling) {
+      return;
+    }
+
     setUpdates((u) => [
       {
         date: new Date().toISOString(),
@@ -28,14 +35,21 @@ export function Log({ data, loading, error }: Props) {
       },
       ...u,
     ]);
-  }, [data]);
+  }, [data, isPolling]);
+
+  function handlePauseResume() {
+    setIsPolling(!isPolling);
+  }
 
   return (
     <div className="justify-start flex-col w-full">
       <div className="flex justify-between">
         <h1 className="text-3xl font-bold">Log</h1>
-        <button className="rounded bg-blue-500 px-4 py-2  m-2 text-white hover:cursor-pointer hover:bg-blue-700">
-          Pause Log
+        <button
+          className="rounded bg-blue-500 px-4 py-2  m-2 text-white hover:cursor-pointer hover:bg-blue-700"
+          onClick={handlePauseResume}
+        >
+          {isPolling ? `Pause Log` : `Resume Log`}
         </button>
       </div>
 
@@ -48,7 +62,7 @@ export function Log({ data, loading, error }: Props) {
               {error && <p>Failed fetching data, retrying in 2s...</p>}
               {data.updates?.length && <p>Updates for: {data.date}</p>}
               {data.updates?.map((stockPrice) => (
-                <p key={`${stockPrice.code}-${stockPrice.price}`}>
+                <p key={`${stockPrice.code}-${v4()}`}>
                   {stockPrice.code}: {stockPrice.price}{" "}
                 </p>
               ))}
