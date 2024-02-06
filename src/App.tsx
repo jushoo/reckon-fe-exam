305d8f4fdf66b848806cbc39d81249c73a1c1688
jushoo <denniel.joshua@pm.me>
2024-02-06 09:15:12 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="font-bold justify-items-center flex-1 align-middle h-full">
+        <h1 className="text-red-500">Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
